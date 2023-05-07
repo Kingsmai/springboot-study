fa955650421f9d91631d7aea524959a94f982005
Xiaomai <xsbugh@gmail.com>
2023-05-07 10:48:21 +0800

    创建项目

---
+++ springboot-study-backend/src/test/java/com/example/springbootstudybackend/SpringbootStudyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
